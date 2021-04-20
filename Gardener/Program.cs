94263f3053958ff2c2f3cb735f7cf29a97a0c3dd
NoraNoctis <32932159+NoraNoctis@users.noreply.github.com>
2021-04-20 17:43:32 +0200

    1

---
+++ Gardener/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gardener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
